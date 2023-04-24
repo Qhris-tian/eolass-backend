283db20ab572a33109b86b70fbe5e489cf0468a3
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-24 07:54:06 +0000

    Update routes.py

--- src/api_v1/routes.py
+++ src/api_v1/routes.py
@@ -4,6 +4,6 @@ from starlette.responses import JSONResponse
 api_router = APIRouter(default_response_class=JSONResponse)
 
 
-@api_router.get("/healthcheck", include_in_schema=False)
+@api_router.get("/health-check", include_in_schema=False)
 def healthcheck():
     return {"status": "ok"}
