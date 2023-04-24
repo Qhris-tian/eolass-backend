d15e122a2964b1ca316ef7005c3c1b81e0012612
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-24 07:54:00 +0000

    Update logging.py

--- src/logging.py
+++ src/logging.py
@@ -2,4 +2,4 @@ import logging
 
 LOG_FORMAT_DEBUG = "%(levelname)s:%(message)s:%(pathname)s:%(funcName)s:%(lineno)d"
 
-logging.basicConfig(format=LOG_FORMAT_DEBUG)
+log = logging.getLogger(__name__)
