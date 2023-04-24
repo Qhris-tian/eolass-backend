96c28e66b7f30b74437c5b3be2d833d48e057a39
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-24 07:54:03 +0000

    Update events.py

--- src/events.py
+++ src/events.py
@@ -1,12 +1,15 @@
+from src.logging import log
+
+
 def start_app_handler():  # pragma: no cover
     """
     Handles process initailization upon application start up.
     """
-    pass
+    log.info("Starting application...")
 
 
 def shutdown_app_handler():  # pragma: no cover
     """
     Handles process initailization upon application shut down.
     """
-    pass
+    log.info("Shutting donw application...")
