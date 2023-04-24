cd847b58882c365a947cc01e51e0fbdbf641f098
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-24 07:53:22 +0000

    Update exception_handlers.py

--- src/api_v1/exception_handlers.py
+++ src/api_v1/exception_handlers.py
@@ -9,7 +9,7 @@ from starlette.responses import JSONResponse
 async def http_exception_handler(
     request: Request,
     exc: HTTPException,
-) -> JSONResponse:
+) -> JSONResponse:  # pragma: no cover
     return JSONResponse(
         {"errors": [exc.detail], "status_code": exc.status_code},
         status_code=exc.status_code,
@@ -19,7 +19,7 @@ async def http_exception_handler(
 async def request_error_handler(
     request: Request,
     exc: RequestValidationError,
-) -> JSONResponse:
+) -> JSONResponse:  # pragma: no cover
     error_bag: Dict = {}
 
     for error in exc.errors():
