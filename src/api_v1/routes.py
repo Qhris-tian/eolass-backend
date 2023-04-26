5039cef1cc52edd3fe21a5c3939013d54fac6b73
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-26 08:13:17 +0000

    updated routes

--- src/api_v1/routes.py
+++ src/api_v1/routes.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, RedirectResponse
 
 from src.api_v1.product.controller import router as product_router
+from src.config import get_settings
 
-api_router = APIRouter(default_response_class=JSONResponse)
+settings = get_settings()
+
+main_router = APIRouter()
+
+api_router = APIRouter(
+    default_response_class=JSONResponse, prefix="/api/{0}".format(settings.API_VERSION)
+)
+
 
 api_router.include_router(
     product_router,
@@ -15,3 +23,11 @@ api_router.include_router(
 @api_router.get("/health-check", include_in_schema=False)
 def healthcheck():
     return {"status": "ok"}
+
+
+main_router.include_router(api_router)
+
+
+@main_router.get("/", include_in_schema=False)  # pragma: no cover
+def root():
+    return RedirectResponse("/redoc")
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
 from src.api_v1.exception_handlers import http_exception_handler, request_error_handler
-from src.api_v1.routes import api_router
+from src.api_v1.routes import main_router
 from src.config import get_settings
 from src.events import shutdown_app_handler, start_app_handler
 
@@ -24,9 +24,7 @@ def create_application() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    application.include_router(
-        api_router, prefix="/api/{0}".format(settings.API_VERSION)
-    )
+    application.include_router(main_router)
     application.add_exception_handler(HTTPException, http_exception_handler)
     application.add_exception_handler(RequestValidationError, request_error_handler)
     application.add_event_handler("startup", start_app_handler)
