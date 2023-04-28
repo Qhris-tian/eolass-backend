afea4e7ef491d716304a7f8a4bb5952d1d34fa55
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-28 14:39:40 +0000

    Update eneba.py

--- src/plugins/eneba.py
+++ src/plugins/eneba.py
@@ -60,6 +60,46 @@ class EnebaClient(BaseClient):
 
         return data["S_products"]
 
+    def get_product(
+        self,
+        product_id: str,
+    ):
+        response = self.post_json(
+            "graphql/",
+            {
+                "query": """query {
+                            S_product(productId: "%s") {
+                                    id
+                                    name
+                                    languages
+                                    regions { code }
+                                    releasedAt
+                                    createdAt
+                                    slug
+                                    type { value }
+                                    auctions(first: 1) {
+                                    edges {
+                                        node {
+                                        belongsToYou
+                                        isInStock
+                                        merchantName
+                                        price { amount currency }
+                                        }
+                                    }
+                                    }
+                            }
+                            }"""
+                % (product_id)
+            },
+        )
+
+        if "errors" in response.json():
+            return None
+
+        data = dict(remove_edges_and_nodes(response.json()["data"]))
+
+        return data["S_product"]
+
 
 @timed_lru_cache(210000)
 def get_access_token() -> str:
