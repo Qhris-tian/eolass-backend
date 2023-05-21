c6241a15e965fbf87c4a6a52aa3f8380849383d4
Qhris-tian <christian.kojobarnes@gmail.com>
2023-05-21 14:09:27 +0000

    Updated error response for rate limit constraint

--- src/api_v1/exception_handlers.py
+++ src/api_v1/exception_handlers.py
@@ -4,6 +4,7 @@ from fastapi import HTTPException, status
 from fastapi.exceptions import RequestValidationError
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from ratelimit.exception import RateLimitException
 
 
 async def http_exception_handler(
@@ -31,4 +32,17 @@ async def request_error_handler(
             "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
         },
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
+
+
+async def  rate_limit_error_handler(
+    request: Request,
+    exception: RateLimitException,
+):
+    return JSONResponse(
+        content={
+            "errors": "Too many request try after again later.",
+            "status_code": status.HTTP_429_TOO_MANY_REQUESTS,
+        },
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
     )
