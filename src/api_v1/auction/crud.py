bdc5faa10c11c513dc7d1c2ea56b1cf81942c8fb
Qhris-tian <christian.kojobarnes@gmail.com>
2023-06-30 17:50:42 +0000

    updates

--- src/api_v1/auction/crud.py
+++ src/api_v1/auction/crud.py
@@ -1,7 +1,19 @@
+from typing import Any, Dict
+
+from fastapi.encoders import jsonable_encoder
+
+from .schema import CreateAuctionInDB
 
 
-async def create_auction_details(auction, db):
-    result = await db["auctions"].insert_one(auction)
-    newly_created_auction = await db["auctions"].find_one({"_id": result.inserted_id})
+async def find_one_auction_by(key: str, value: Any, db):
+    auction = await db["auctions"].find_one({key: str(value)})
 
-    return newly_created_auction
+    return auction
+
+
+async def create_auction_details(auction: Dict, db):
+    data = CreateAuctionInDB(**auction)
+    result = await db["auctions"].insert_one(jsonable_encoder(data))
+    created_auction = await find_one_auction_by("_id", result.inserted_id, db)
+
+    return created_auction
